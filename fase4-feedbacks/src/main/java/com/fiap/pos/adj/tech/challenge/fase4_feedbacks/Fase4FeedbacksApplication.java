b48fc3f856649b130802cc76e536575fa1edd32e
juniorsmartins <junior.soares.martins@proton.me>
2025-10-06 15:17:54 -0400

    Second Commit

---
+++ fase4-feedbacks/src/main/java/com/fiap/pos/adj/tech/challenge/fase4_feedbacks/Fase4FeedbacksApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.pos.adj.tech.challenge.fase4_feedbacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fase4FeedbacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fase4FeedbacksApplication.class, args);
+	}
+
+}
