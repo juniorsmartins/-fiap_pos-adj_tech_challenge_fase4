e77e70b593cfec3dcc6ffdecb66d2deee9752ebb
juniorsmartins <junior.soares.martins@proton.me>
2025-10-17 10:52:52 -0400

    FEAT: Kafka.

--- fase4-users/src/main/java/com/fiap/pos/adj/tech/challenge/fase4_users/infrastructure/kafka/producer/KafkaProducer.java
+++ fase4-users/src/main/java/com/fiap/pos/adj/tech/challenge/fase4_users/infrastructure/kafka/producer/KafkaProducer.java
@@ -1,25 +1,8 @@
 package com.fiap.pos.adj.tech.challenge.fase4_users.infrastructure.kafka.producer;
 
-import com.fiap.pos.adj.tech.challenge.fase4_users.application.configs.kafka.kafkaPropertiesConfig;
 import com.fiap.pos.adj.tech.challenge.fase4_users.application.dtos.response.EstudanteKafka;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
 
-import java.util.UUID;
+public interface KafkaProducer {
 
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public final class KafkaProducer {
-
-    private final KafkaTemplate<String, Object> kafkaTemplate;
-
-    private final kafkaPropertiesConfig kafkaPropertiesConfig;
-
-    public void enviarEventoUsers(EstudanteKafka estudanteKafka) {
-        kafkaTemplate.send(kafkaPropertiesConfig.topicEventCreateUsers, UUID.randomUUID().toString(), estudanteKafka);
-        log.info("\n\n enviarEventoUsers - Mensagem enviada ao tópico de eventos: {}. \n\n", estudanteKafka);
-    }
+    void enviarEventoUsers(EstudanteKafka kafka);
 }
---
+++ fase4-users/src/main/java/com/fiap/pos/adj/tech/challenge/fase4_users/infrastructure/kafka/producer/KafkaProducerImpl.java
@@ -0,0 +1,26 @@
+package com.fiap.pos.adj.tech.challenge.fase4_users.infrastructure.kafka.producer;
+
+import com.fiap.pos.adj.tech.challenge.fase4_users.application.configs.kafka.kafkaPropertiesConfig;
+import com.fiap.pos.adj.tech.challenge.fase4_users.application.dtos.response.EstudanteKafka;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public final class KafkaProducerImpl implements KafkaProducer {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    private final kafkaPropertiesConfig kafkaPropertiesConfig;
+
+    @Override
+    public void enviarEventoUsers(EstudanteKafka estudanteKafka) {
+        kafkaTemplate.send(kafkaPropertiesConfig.topicEventCreateUsers, UUID.randomUUID().toString(), estudanteKafka);
+        log.info("\n\n enviarEventoUsers - Mensagem enviada ao tópico de eventos: {}. \n\n", estudanteKafka);
+    }
+}
